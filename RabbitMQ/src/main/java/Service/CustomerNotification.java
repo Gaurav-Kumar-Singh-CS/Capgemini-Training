264e97e00b8930ae7a6d8bc5da2d3999b76ff317
Gaurav-Kumar-Singh-CS <71564989+Gaurav-Kumar-Singh-CS@users.noreply.github.com>
2021-03-31 21:38:57 +0530

    RabbitMQ assignment

---
+++ RabbitMQ/src/main/java/Service/CustomerNotification.java
@@ -0,0 +1,21 @@
+package com.assignment1.CustomerProd.Service;
+
+import com.assignment1.CustomerProd.Model.Customer;
+import com.assignment1.CustomerProd.RabbitMQConfig;
+import com.assignment1.CustomerProd.Repo.CustomerRepo;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerNotification {
+
+    @Autowired
+    CustomerRepo customerRepo;
+
+    @RabbitListener(queues = RabbitMQConfig.queueName)
+    public void consumeMess(Customer customer){
+        customerRepo.save(customer);
+        System.out.println("Customer added to DB");
+    }
+}
